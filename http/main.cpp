83d174280af702f9a9a755c66ed55a5f3e0244a7
JHLeeeMe <lejung92@gmail.com>
2022-11-11 19:34:00 +0900

    [Update] main() for echo
    	수정함:        http/main.cpp

--- http/main.cpp
+++ http/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <memory.h>
+#include <unistd.h>
 
 #include "net/http_interface.h"
 #include "net/http_server.h"
@@ -6,12 +8,33 @@
 
 int main()
 {
+    const char* localhost = "127.0.0.1";
+    const int   port      = 7777;
+
     //jhleeeme::net::IHttp* http_server = new jhleeeme::net::HttpServer();
     //http_server->create_socket();
     //std::cout << http_server->get_sockfd() << std::endl;
     auto http_server = jhleeeme::net::HttpServer();
     http_server.create_socket();
-    std::cout << http_server.get_sockfd() << std::endl;
+    http_server.set_sockaddr(localhost, port);
+    http_server.bind();
+    http_server.listen();
+
+    std::cout << "Waiting..." << std::endl;
+
+    int client_socket = http_server.accept();
+    std::cout << "connected." << std::endl;
+    http_server.close();
+
+    char buf[4096];
+    memset(buf, 0x00, 4096);
+
+    recv(client_socket, buf, 4096, 0);
+    std::cout << buf << std::endl;
+
+    close(client_socket);
+
+    std::cout << "Server end." << std::endl;
 
     return 0;
 }
