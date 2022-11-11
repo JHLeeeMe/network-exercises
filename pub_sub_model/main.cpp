e0f719b109b0a5709e35d892181290e6377eee18
JHLeeeMe <lejung92@gmail.com>
2022-11-11 22:19:31 +0900

    [Add] ```Pub/Sub model``` in multi-threads
    	새 파일:       pub_sub_model/Makefile
    	새 파일:       pub_sub_model/main.cpp

---
+++ pub_sub_model/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <chrono>
+
+#define __PRODUCER_SIZE (10)
+#define __CONSUMER_SIZE (1)
+
+
+void producer(std::queue<int>* task_queue, std::mutex& mtx, int idx)
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> key(mtx);
+        if (task_queue->size() < __PRODUCER_SIZE)
+        {
+            task_queue->push(idx);
+        }
+        key.unlock();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
+void consumer(std::queue<int>* task_queue, std::mutex& mtx)
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> key(mtx);
+        if (!task_queue->empty())
+        {
+            int task = task_queue->front();
+            printf("task: %d, queue size: %ld\n", task, task_queue->size());
+            task_queue->pop();
+        }
+        key.unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+}
+
+int main()
+{
+    std::queue<int> task_queue;
+    std::mutex mtx;
+
+    std::vector<std::thread> producers;
+    for (int i = 0; i < __PRODUCER_SIZE; i++)
+    {
+        producers.push_back(
+            std::thread(producer, &task_queue, std::ref(mtx), i));
+    }
+
+    std::vector<std::thread> consumers;
+    for (int i = 0; i < __CONSUMER_SIZE; i++)
+    {
+        consumers.push_back(
+            std::thread(consumer, &task_queue, std::ref(mtx)));
+    }
+
+    for (auto& p : producers)
+    {
+        p.join();
+    }
+    for (auto& c : consumers)
+    {
+        c.join();
+    }
+
+    return 0;
+}
