4a56b08d132ff821932816390f1600141502d99d
JHLeeeMe <lejung92@gmail.com>
2022-11-17 21:47:43 +0900

    [Update] thread 관련 코드 간단한거 추가
    	수정함:        http/main.cpp

--- http/main.cpp
+++ http/main.cpp
@@ -1,38 +1,52 @@
 #include <iostream>
 #include <memory.h>
 #include <unistd.h>
+#include <vector>
+#include <thread>
+#include <mutex>
 
 #include "net/http_interface.h"
 #include "net/http_server.h"
 #include "net/http_client.h"
+
+
+void connection(int&& sockfd, std::mutex& mtx)
+{
+    std::cout << "connected." << std::endl;
+
+    char buf[64];
+    while (true)
+    {
+        memset(buf, 0x00, 64);
+
+        recv(sockfd, buf, 64, 0);
+        std::cout << buf;
+    }
+    close(sockfd);
+}
 
 int main()
 {
     const char* localhost = "127.0.0.1";
     const int   port      = 7777;
 
-    //jhleeeme::net::IHttp* http_server = new jhleeeme::net::HttpServer();
-    //http_server->create_socket();
-    //std::cout << http_server->get_sockfd() << std::endl;
+    std::vector<std::thread> conn_threads;
+    conn_threads.reserve(10);
+    std::mutex mtx;
+
     auto http_server = jhleeeme::net::HttpServer();
     http_server.create_socket();
     http_server.set_sockaddr(localhost, port);
     http_server.bind();
     http_server.listen();
 
-    std::cout << "Waiting..." << std::endl;
-
-    int client_socket = http_server.accept();
-    std::cout << "connected." << std::endl;
-    http_server.close();
-
-    char buf[4096];
-    memset(buf, 0x00, 4096);
-
-    recv(client_socket, buf, 4096, 0);
-    std::cout << buf << std::endl;
-
-    close(client_socket);
+    while (true)
+    {
+        int client_socket = http_server.accept();
+        conn_threads.push_back(
+            std::thread(connection, std::move(client_socket), std::ref(mtx))
+        );
+    }
 
     std::cout << "Server end." << std::endl;
 
